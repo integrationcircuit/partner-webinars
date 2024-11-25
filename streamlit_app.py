2153aad1d133c0bfd8ead676466e931a885f6ab4
integrationcircuit <163421937+integrationcircuit@users.noreply.github.com>
2024-11-25 11:26:13 +0000

    minor updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import requests
 
 def show_registration_form():
-    session_1_date = "Platform Deep Dive (Dec 13, 9:30 AM CET)"
-    session_2_date = "GenAI Solutions (Dec 20, 9:30 AM CET)"
+    session_1_date = "The Foundations(Dec 13, 9:30 AM CET)"
+    session_2_date = "Generative Integration (Dec 20, 9:30 AM CET)"
     
     st.subheader("Webinar Registration")
     
